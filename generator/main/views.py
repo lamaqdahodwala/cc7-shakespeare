df6cd2e499d409aca4470e3169aba158d7beba43
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-06-22 11:10:03 -0400

    finished functionality

--- generator/main/views.py
+++ generator/main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
 def index(req):
     return render(req, 'index.html')
 
 def generate_insult(req):
-    return render(req, 'insult.html')
+    r = requests.get('https://insultapi.vercel.app/api/generated')
+    insult = r.text
+    return render(req, 'insult.html', {'insult': insult})
