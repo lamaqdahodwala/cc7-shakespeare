614deb10d5499901858bd20c3a646f994336fbcc
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-06-22 11:03:33 -0400

    created requirements.txt

--- generator/main/urls.py
+++ generator/main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index')
+   #  path('insult', views.generate_insult, name='insult'))
 ]
--- generator/main/views.py
+++ generator/main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(req):
     return render(req, 'index.html')
+
+def generate_insult(req):
+    return render(req, 'insult.html')
